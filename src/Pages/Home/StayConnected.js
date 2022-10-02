c894a836dcfe83e2ad2475311a45683bd5abe902
Jobayer_ak <jobayer.ak@gmail.com>
2022-10-02 13:07:14 +0600

    stay conected form is donee

--- src/Pages/Home/StayConnected.js
+++ src/Pages/Home/StayConnected.js
@@ -1,46 +1,54 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import appoinment from "../../assets/images/appointment.png";
 import Button from "../Shared/Button";
 
 const StayConnected = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <section
       style={{
         background: `url(${appoinment})`,
         backgroundSize: "cover",
       }}
-      className="text-center mt-12 p-12">
+      className="text-center mt-12 p-12"
+    >
       <h2 className="text-primary text-2xl">Contact Us</h2>
       <h2 className="text-4xl text-white font-bold my-5">
         Stay Connected With Us
       </h2>
-      <div className="w-full max-w-lg mx-auto mt-10">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-              id="username"
-              type="email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight"
-              id=""
-              type="text"
-              placeholder="Subject"
-            />
-          </div>
-          <div className="mb-6">
-            <textarea
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight "
-              id=""
-              type="text"
-              placeholder="Subject"
-            />
-          </div>
-          <div className="flex items-center justify-center">
+      <div className="w-72 lg:w-96 mx-auto mt-10">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("Email", { required: true })}
+            type="email"
+            required
+            placeholder="Email Address"
+            className="input w-full"
+          />
+          <input
+            {...register("Subject", { required: true })}
+            type="text"
+            required
+            placeholder="Subject"
+            className="input w-full my-4"
+          />
+          <textarea
+            {...register("message", { required: true, maxLength: 1000 })}
+            type="text"
+            rows={5}
+            required
+            placeholder="Write your message"
+            className="w-full textarea"
+          />
+
+          <div className="flex items-center justify-center mt-4">
             <Button>Submit</Button>
           </div>
         </form>
--- src/Pages/Shared/Button.js
+++ src/Pages/Shared/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({children}) => {
     return (
         <div>
-            <button className="btn btn-primary uppercase bg-gradient-to-r from-secondary to-primary">{children}</button>
+            <button className="btn btn-primary uppercase bg-gradient-to-r from-secondary to-primary text-white">{children}</button>
         </div>
     );
 };
