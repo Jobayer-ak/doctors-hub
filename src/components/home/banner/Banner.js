72d5f9e3b0a701e1f3f5eb93991c2b71a9075147
Jobayer_ak <jobayer.ak@gmail.com>
2023-01-15 20:58:23 +0600

    banner part responsive is done

--- src/components/home/banner/Banner.js
+++ src/components/home/banner/Banner.js
@@ -4,7 +4,7 @@ import Regions from "./region/Regions";
 
 const Banner = () => {
   return (
-    <div id="banner">
+    <div id="banner" className="bg-[#23075E] py-2.5 rounded-md">
       <h1 className="font-bold text-4xl text-center">
         Meet <span className="text-[#D3ADF8]">Doctor</span> In Your City
       </h1>
--- src/components/home/banner/region/Regions.js
+++ src/components/home/banner/region/Regions.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./regions.css";
 import Region from "./Region";
 
 const Regions = () => {
   const regions = ["Dhaka", "Chottogram", "Mymensingh", "Sylhet"];
   return (
-    <div className="md:flex md:justify-between md:items-cente px-4 pt-3 md:pt-0 md:py-7">
+    <div className="md:flex md:justify-between md:items-cente px-4 pt-3 md:pt-7 md:pb-7">
       {regions?.map((region, index) => (
         <Region key={index} region={region} />
       ))}
