abce628098a9dd8e665e4e67bf46404e149cd8ce
Jobayer_ak <jobayer.ak@gmail.com>
2023-02-09 00:04:30 +0600

    user can delete single booking

--- src/components/Dashboard/AppointmentsHistory.js
+++ src/components/Dashboard/AppointmentsHistory.js
@@ -3,6 +3,9 @@ import AuthContext from "../../context/AuthProvider";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { format } from "date-fns";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import Swal from "sweetalert2";
 
 const MyHistory = () => {
   const { user } = useContext(AuthContext);
@@ -22,28 +25,68 @@ const MyHistory = () => {
     return <h2 className="text-xl font-bold text-white">Loading......</h2>;
   }
 
+  // console.log(data);
+
+  const handleDelete = (book_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await axios
+          .delete(`http://localhost:5000/api/v1/booking/delete/${book_id}`, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            if (res.data.success === true) {
+              console.log(book_id);
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+              refetch();
+            }
+          })
+          .catch((err) => console.log(err));
+      }
+    });
+  };
 
   return (
     <div className="px-4">
       <div className="overflow-x-auto">
         <table className="table w-full md:min-w-[60%] lg:w-full">
           <thead>
-            <tr>
+            <tr className="text-center">
               <th>Sr.</th>
               <th>Doctor</th>
               <th>Date</th>
               <th>Time</th>
               <th>Specialist</th>
+              <th>Remove</th>
             </tr>
           </thead>
           <tbody>
             {data?.map((a, index) => (
-              <tr className="relative">
+              <tr className="relative text-center">
                 <th className="sticky left-0">{index + 1}</th>
                 <td>{a.doctor_name}</td>
                 <td>{format(new Date(a.date), "PP")}</td>
                 <td>{a.slot}</td>
                 <td>{a.speciality}</td>
+                <td
+                  className="cursor-pointer"
+                  onClick={() => handleDelete(a._id)}
+                >
+                  {
+                    <FontAwesomeIcon
+                      icon={faTrashCan}
+                      className="bg-red-700 px-2 py-2 rounded-md text-white"
+                    />
+                  }
+                </td>
               </tr>
             ))}
           </tbody>
