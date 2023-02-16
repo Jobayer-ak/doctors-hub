9f0a732d860c70dc1c83849fc23657d81d8caf31
Jobayer_ak <jobayer.ak@gmail.com>
2023-02-16 23:35:15 +0600

    added modal for update profile on setting

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -23,7 +23,7 @@ const Setting = () => {
     reset,
   } = useForm();
   const { user } = useContext(AuthContext);
-  const [open, setOpen] = useState(false);
+ 
 
   const initialInputValues = {
     name: "",
@@ -51,17 +51,17 @@ const Setting = () => {
     return <h2 className="text-xl font-black">Loading...</h2>;
   }
 
-  const createdDate = new Date(data.user.createdAt);
-
+  
   const { email, gender, mobile, name } = data.user;
+  const createdDate = new Date(data.user.createdAt);
 
   const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
-    <div className="w-full min-h-screen lg:h-auto my-0 bg-[#23075e] lg:border-l-4 lg:border-l-4 border-solid border-[#722ED1] z-10 lg:pb-5 pt-6 px-10">
-      <div className="bg-gradient-to-r from-slate-800 to-indigo-800 to-indigo-600 p-10 rounded-sm">
+    <div className="w-full min-h-screen lg:h-auto my-0 bg-[#23075e] lg:border-l-4 lg:border-l-4 border-solid border-[#722ED1] z-10 lg:pb-5 pt-6 px-5 md:px-10">
+      <div className="bg-gradient-to-r from-slate-800 to-indigo-800 to-indigo-600 p-4 md:px-10 rounded-sm">
         <h2 className="text-white font-bold text-2xl text-center mb-6">
           My Account Information
         </h2>
@@ -83,13 +83,15 @@ const Setting = () => {
           As user since:{" "}
           <span className="ml-4 text-[#a8a29e]">
             {format(createdDate, "PP")}
-          </span>{" "}
+          </span>
         </p>
 
-        <div className="text-center">
-          <label htmlFor="my-modal-3" className="btn">
+        <div className="text-center my-6">
+          <label htmlFor="my-modal-3" className="btn bg-[#0a062c] rounded-md mr-5">
             update profile
           </label>
+
+          <button className="btn bg-[#0a062c] rounded-md">Delete Your Account</button>
         </div>
       </div>
 
@@ -139,18 +141,7 @@ const Setting = () => {
                     <input
                       className="text-center p-2 w-full max-w-sm focus:bg-[#722ed1] border-none outline-0 rounded-sm"
                       type="text"
-                      value={values.email}
-                      // // readOnly
-                      // {...register("email", {
-                      //   required: {
-                      //     value: true,
-                      //     message: "Email is required!",
-                      //   },
-                      //   pattern: {
-                      //     value: /[a - z0 - 9] + @[a - z] +\.[a-z]{2, 3}/,
-                      //     message: "Provide a valid email!",
-                      //   },
-                      // })}
+                      value={email}
                     />
                   </div>
 
@@ -185,7 +176,7 @@ const Setting = () => {
                         placeholder="Gender"
                         value="Male"
                         className=""
-                        checked={gender}
+                        checked
                         {...register("gender", {
                           required: {
                             value: true,
@@ -200,6 +191,7 @@ const Setting = () => {
                       <input
                         type="radio"
                         value="Female"
+                        
                         {...register("gender", {
                           required: {
                             value: true,
@@ -219,134 +211,9 @@ const Setting = () => {
                 </form>
               </div>
             </div>
-            {/* booking form  */}
           </div>
         </div>
       </div>
-
-      {/* <div className={open === true?  "px-7 md:pt-12 pb-2.5 text-center block": "hidden"}>
-        <h2 className="text-[#722ed1] font-bold text-3xl md:text-4xl bg-white inline-block py-3 px-6 rounded-full">
-          <span className="mr-4">Update Profile</span>
-          <FontAwesomeIcon icon={faCircleUser} size="xl" />
-        </h2>
-
-        <form
-          className="mt-12 md:bg-[#23075e] text-center relative"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-         
-          <div className="">
-            <FontAwesomeIcon
-              className="p-2.5 absolute text-[#23075e]"
-              icon={faUserPlus}
-              size="lg"
-            />
-            <input
-              className="text-center p-2 w-full max-w-sm focus:bg-[#722ed1] border-none outline-0 rounded-sm"
-              type="text"
-              placeholder={name}
-              
-              value={values.name}
-              {...register("name", {
-                required: {
-                  value: true,
-                  message: "Name is required!",
-                },
-              })}
-            />
-          </div>
-
-          
-          <div className="my-8">
-            <FontAwesomeIcon
-              className="p-2.5 absolute text-[#23075e]"
-              icon={faEnvelope}
-              size="lg"
-            />
-            <input
-              className="text-center p-2 w-full max-w-sm focus:bg-[#722ed1] border-none outline-0 rounded-sm"
-              type="text"
-              value={values.email}
-              // readOnly
-              {...register("email", {
-                required: {
-                  value: true,
-                  message: "Email is required!",
-                },
-                pattern: {
-                  value: /[a - z0 - 9] + @[a - z] +\.[a-z]{2, 3}/,
-                  message: "Provide a valid email!",
-                },
-              })}
-            />
-          </div>
-
-          
-          <div className="my-8">
-            <FontAwesomeIcon
-              className="p-2.5 absolute text-[#23075e]"
-              icon={faPhone}
-              size="lg"
-            />
-            <input
-              className="text-center p-2 w-full max-w-sm focus:bg-[#722ed1] border-none outline-0 rounded-sm"
-              type="text"
-              placeholder={mobile}
-              value={values.mobile}
-              {...register("mobile", {
-                required: {
-                  value: true,
-                  message: "Mobile number is required!",
-                },
-              })}
-            />
-          </div>
-
-          
-          <div className="text-white mb-8 flex justify-center items-center gap-[50px] md:gap-[75px]">
-            <div>
-              <h3 className="font-bold">Gender</h3>
-            </div>
-
-            <div className="flex items-center gap-3">
-              <input
-                type="radio"
-                placeholder="Gender"
-                value="Male"
-                className=""
-                checked={gender}
-                {...register("gender", {
-                  required: {
-                    value: true,
-                    message: "Gender is required!",
-                  },
-                })}
-              />
-              <p>Male</p>
-            </div>
-
-            <div className="flex items-center gap-3">
-              <input
-                type="radio"
-                value="Female"
-                {...register("gender", {
-                  required: {
-                    value: true,
-                    message: "Gender is required!",
-                  },
-                })}
-              />
-              <p>Female</p>
-            </div>
-          </div>
-
-          <input
-            className="text-center text-white font-bold bg-[#722ed1] hover:bg-[#9258e5] transition-all p-2 w-full max-w-sm cursor-pointer rounded-sm"
-            type="submit"
-            value="Save"
-          />
-        </form>
-      </div> */}
     </div>
   );
 };
