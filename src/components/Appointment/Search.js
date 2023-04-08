2a59bd458ac0a1ba5ead885c38ecca10fdff015b
Jobayer_ak <jobayer.ak@gmail.com>
2023-04-09 05:03:31 +0600

    profile part static design is done with notification icon

--- src/components/Appointment/Search.js
+++ src/components/Appointment/Search.js
@@ -1,31 +1,58 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import image1 from '../../assets/home/im1.jpg';
+import profile from '../../assets/images/avatar.png';
+import notification from '../../assets/home/notification.gif';
 
 const Search = () => {
+  const a = 3;
   return (
-    <div className="lg:flex lg:gap-12 lg:justify-between lg:items-center mt-7 mb-9 md:w-[100%] lg:w-full">
-      <div className="search flex justify-between items-center bg-[#23075e] w-full rounded-md py-2.5 px-2.5">
-        <h2 className="text-xl font-bold">Home</h2>
-
-        <form className="flex items-center bg-white rounded-md text-black">
-          <input
-            type="text"
-            className="px-1.5 py-1 w-full max-w-sm border-none outline-0 rounded-md"
-            placeholder="Search"
-          />
-          <button type="submit">
-            <FontAwesomeIcon
-              icon={faSearch}
-              size="lg"
-              className="px-2 text-slate-600"
+    <div className="w-full bg-[#23075e] mb-1">
+      <div className="lg:flex lg:gap-1 lg:justify-between lg:items-center">
+        {/* Search */}
+        <div className="w-[60%] px-3 py-4 border-r-4 border-[#090629]">
+          <form className="flex justify-between items-center bg-white rounded-sm text-black">
+            <input
+              type="text"
+              className="px-2 py-4 w-full border-none outline-0 focus:bg-slate-500 focus:text-white"
+              placeholder="Search Doctor"
             />
-          </button>
-        </form>
-      </div>
+            <button type="submit" className="">
+              <FontAwesomeIcon
+                icon={faSearch}
+                size="2xl"
+                className="px-2 text-slate-700"
+              />
+            </button>
+          </form>
+        </div>
 
-      <div className="bg-[#4b1b97] hidden lg:block py-3 px-[15px] w-[350px] rounded-md font-bold text-xl mt-5 md:mt-0">
-        Profile
+        {/* user profile */}
+        <div className="w-[40%] text-white px-4 py-0 flex justify-center gap-4 items-center">
+          {a !== 3 ? (
+            <img src={profile} alt="" className="rounded-full w-[15%]" />
+          ) : (
+            <img src={image1} alt="" className="rounded-full w-[15%]" />
+          )}
+          <div className="">
+            {a !== 3 ? (
+              <h3 className="font-bold text-[#f68685] text-xl">User Profile</h3>
+            ) : (
+              <div className='flex justify-between items-center gap-8'>
+                <div className=''>
+                  <h3 className="font-bold text-[#f68685] text-xl">
+                    Md. Jobayer Akanda
+                  </h3>
+                  <p className="text-white italic">jobayer.ak@gmail.com</p>
+                </div>
+                <div>
+                  <img src={notification} alt="" className="rounded-full" />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
