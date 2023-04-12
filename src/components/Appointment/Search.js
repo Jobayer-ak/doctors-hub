4afd64d70989ca88223b6d5af580bf04850bafc4
Jobayer_ak <jobayer.ak@gmail.com>
2023-04-13 03:28:33 +0600

    according to search input value user will get suggestion and select specific item id done

--- src/components/Appointment/Search.js
+++ src/components/Appointment/Search.js
@@ -10,7 +10,7 @@ import useStorage from '../../hook/useStorage';
 import { useNavigate } from 'react-router-dom';
 import { Dna } from 'react-loader-spinner';
 import SearchResult from './SearchResult';
-
+import Loader from '../common/Loading/Loader';
 
 const Search = ({ newBooking }) => {
   const date = new Date();
@@ -18,8 +18,10 @@ const Search = ({ newBooking }) => {
   const [user] = useStorage();
   const navigate = useNavigate();
   const userInfo = JSON.parse(user);
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState('');
   const url = '/pending-appointments';
+  const [getDoctors, setGetDoctors] = useState([]);
+  const [result, setResult] = useState('');
 
   const { data, isLoading, isError } = useQuery(
     ['profileInfo', newBooking],
@@ -36,33 +38,43 @@ const Search = ({ newBooking }) => {
     }
   );
 
-  const { data: doc, isLoading: docLoading, isError: docError } = useQuery(
-    ['getAllDocs'],
-    async () => {
-      const res = await baseURL.get(
-        `${url}?patient=${userInfo.email}&&date=${formattedDate}`,
-        {
-          withCredentials: true,
-        }
-      );
+  const {
+    data: doctors,
+    isLoading: docLoading,
+    isError: docError,
+  } = useQuery(['getAllDocs'], async () => {
+    const res = await baseURL.get(`/search-doctors`);
 
-      const result = res.data;
-      return result;
-    }
-  );
+    const result = res.data;
+    setGetDoctors(result.doctors);
+    return result;
+  });
 
+  if (docLoading) {
+    return <Loader />;
+  }
 
   const handleChange = (e) => {
     e.preventDefault();
     setSearchValue(e.target.value);
-  }
+
+    // filtering
+    const filteredData = getDoctors?.filter((item) =>
+      item.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setResult(filteredData);
+  };
+
+  const onSearch = (searchTerm) => {
+    setSearchValue(searchTerm);
+  };
 
   return (
     <div className="w-full bg-[#23075e] mb-1">
       <div className="md:flex md:gap-1 md:justify-between lg:items-center">
         {/* Search */}
         <div className="w-full md:w-[55%] lg:w-[60%] px-3 py-4">
-          <form className="flex justify-between items-center bg-white rounded-sm text-black">
+          <div className="flex justify-between items-center bg-white rounded-sm text-black">
             <input
               type="text"
               className="px-2 py-3 w-full border-none outline-0 focus:bg-slate-500 focus:text-white"
@@ -70,22 +82,16 @@ const Search = ({ newBooking }) => {
               value={searchValue}
               onChange={handleChange}
             />
-            <button type="submit" className="">
+            <button type="submit" onClick={() => onSearch(searchValue)}>
               <FontAwesomeIcon
                 icon={faSearch}
                 size="2xl"
                 className="px-2 text-slate-700"
               />
             </button>
-          </form>
+          </div>
         </div>
 
-        <div>
-          {/* <p className='text-white text-xl'>{searchValue}</p> */}
-        </div>
-        {/* search result shown */}
-        {/* <SearchResult /> */}
-        
         {/* user profile */}
         {isLoading ? (
           <div className="flex justify-center items-center bg-[#23075e] mt-1 w-[40%]">
@@ -99,7 +105,7 @@ const Search = ({ newBooking }) => {
             />
           </div>
         ) : (
-          <div className="w-full lg:w-[40%] hidden text-white md:flex md:justify-center md:gap-4 items-center ">
+          <div className="w-full xl:w-[40%] hidden text-white md:flex md:justify-center md:gap-4 items-center ">
             {isError === true ? (
               <div>
                 <img src={profile} alt="" className="rounded-full w-[15%]" />
@@ -142,6 +148,29 @@ const Search = ({ newBooking }) => {
             </div>
           </div>
         )}
+      </div>
+
+      {/* search result */}
+      <div className="absolute ml-3 mr-3 px-2 bg-slate-600 w-[94%] md:w-[48%] rounded-sm z-50">
+        {result.length > 0 &&
+          result
+            .filter((item) => {
+              const searchTerm = searchValue.toLowerCase();
+              const res = item.name.toLowerCase();
+              return (
+                searchTerm && res.includes(searchTerm) && res !== searchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((item, i) => (
+              <p
+                onClick={() => onSearch(item.name)}
+                className=" text-white cursor-pointer"
+                key={i}
+              >
+                {item.name}
+              </p>
+            ))}
       </div>
     </div>
   );
